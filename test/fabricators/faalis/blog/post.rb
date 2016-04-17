398c7351a0a5647da73cc29ad9345e10a927b732
Sameer Rahmani <lxsameer@gnu.org>
2016-04-17 14:48:19 +0430

    category tests finished

--- test/fabricators/faalis/blog/post.rb
+++ test/fabricators/faalis/blog/post.rb
@@ -1,4 +1,4 @@
-Fabricator :category, class_name: 'Faalis::Blog::Category' do
+Fabricator :post, class_name: 'Faalis::Blog::Post' do
   title 'category_1'
   raw_content '#test1'
   permalink 'category-1'
--- test/models/faalis/blog/category_test.rb
+++ test/models/faalis/blog/category_test.rb
@@ -17,6 +17,35 @@ require 'test_helper'
 module Faalis::Blog
   class CategoryTest < ActiveSupport::TestCase
 
+    @@subject = ::Faalis::Blog::Category
 
+    test "won't save without a title" do
+      subject = @@subject.new(permalink: 'category-1')
+
+      result = subject.save
+
+      assert_not result, msg: 'Category saved without a title.'
+    end
+
+    test "won't save without a permalink" do
+      subject = @@subject.new(title: 'category-1')
+
+      result = subject.save
+
+      assert_not result, msg: 'Category saved without a title.'
+    end
+
+    describe 'published_posts' do
+      test 'will return the published posts belongs to current category' do
+        subject = Fabricate(:category)
+
+        3.times { Fabricate(:post, category: subject, published: true) }
+        Fabricate(:post, category: subject, published: false)
+
+        result = subject.published_posts
+
+        assert_equal 3, result.count
+      end
+    end
   end
 end
