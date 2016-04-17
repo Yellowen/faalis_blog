dbed7f2a8b564043905e2ed8d09f8b249397389c
Sameer Rahmani <lxsameer@gnu.org>
2016-04-17 14:04:48 +0430

    post migration improved to contain more default values

--- db/migrate/20160219191902_create_faalis_blog_posts.rb
+++ db/migrate/20160219191902_create_faalis_blog_posts.rb
@@ -4,12 +4,12 @@ class CreateFaalisBlogPosts < ActiveRecord::Migration
       t.string :title
       t.text   :raw_content
       t.text   :content
-      t.integer :category_id
+      t.integer :category_id, null: false
       t.boolean :published
-      t.integer :user_id
-      t.integer :views
-      t.integer :likes
-      t.integer :dislikes
+      t.integer :user_id,  null: false
+      t.integer :views,    default: 0
+      t.integer :likes,    default: 0
+      t.integer :dislikes, default: 0
       t.boolean :allow_comments, default: true
       t.boolean :members_only, default: false
 
