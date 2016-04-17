c454a9fe29aae814198419edf80f8ec48b3e698a
Sameer Rahmani <lxsameer@gnu.org>
2016-04-17 14:04:01 +0430

    attributes annotation added to models

--- app/models/faalis/blog/category.rb
+++ app/models/faalis/blog/category.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: faalis_blog_categories
+#
+#  id           :integer          not null, primary key
+#  title        :string
+#  description  :text
+#  permalink    :string
+#  members_only :boolean          default(FALSE)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  domain_id    :integer
+#
+
 module Faalis::Blog
   class Category < ActiveRecord::Base
     include ::Faalis::Concerns::Authorizable
--- app/models/faalis/blog/post.rb
+++ app/models/faalis/blog/post.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: faalis_blog_posts
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  raw_content      :text
+#  content          :text
+#  category_id      :integer
+#  published        :boolean
+#  user_id          :integer
+#  views            :integer
+#  likes            :integer
+#  dislikes         :integer
+#  allow_comments   :boolean          default(TRUE)
+#  members_only     :boolean          default(FALSE)
+#  meta_title       :string
+#  meta_description :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  domain_id        :integer
+#
+
 module Faalis::Blog
   class Post < ActiveRecord::Base
     include ::Faalis::Concerns::Authorizable
