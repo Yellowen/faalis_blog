3cf1f963df7c1461bb9b6f25a9899d2b3d740836
Sameer Rahmani <lxsameer@gnu.org>
2016-04-17 14:03:21 +0430

    model's fabricators added

---
+++ test/fabricators/faalis/blog/category.rb
@@ -0,0 +1,6 @@
+Fabricator :category, class_name: 'Faalis::Blog::Category' do
+  title 'category_1'
+  description 'sme desc'
+  permalink 'category-1'
+  members_only  false
+end
---
+++ test/fabricators/faalis/blog/post.rb
@@ -0,0 +1,12 @@
+Fabricator :category, class_name: 'Faalis::Blog::Category' do
+  title 'category_1'
+  raw_content '#test1'
+  permalink 'category-1'
+  members_only  false
+  published false
+  views 0
+  likes 0
+  dislikes 0
+  allow_comments true
+  category { Fabricate(:category) }
+end
