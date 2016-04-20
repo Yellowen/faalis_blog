f3981c95915f67596e650528d5da5af1d0ed4a7b
Sameer Rahmani <lxsameer@gnu.org>
2016-04-20 17:20:38 +0430

    index attributes of post's dashboard interface is done

--- app/controllers/faalis/blog/dashboard/posts_controller.rb
+++ app/controllers/faalis/blog/dashboard/posts_controller.rb
@@ -1,6 +1,10 @@
 class Faalis::Blog::Dashboard::PostsController < ::Dashboard::ApplicationController
   engine 'Faalis::Blog::Engine'
 
+  in_index do |f|
+    f.attributes(:title, :permalink, :category, :domain, :members_only, :allow_comments)
+  end
+
   in_form do |f|
     f.attributes(:title, :permalink, :meta_title, :meta_description,
                  :category, :domain, :raw_content, :tags,
