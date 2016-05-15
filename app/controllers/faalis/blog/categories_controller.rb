478f31679c3e1c72075323de9e21cc2c30007efe
sadmad <sarmad.re@gmail.com>
2016-05-15 17:01:02 +0430

    made blog controller category and posts

--- app/controllers/faalis/blog/categories_controller.rb
+++ app/controllers/faalis/blog/categories_controller.rb
@@ -6,7 +6,21 @@ module Faalis::Blog
     layout Engine.default_layout
 
     def show
-      @category = Category.find_by(permalink: params[:permalink])
+      categoris = Category.published
+
+      if user_signed_in?
+         @category = categories.find_by(permalink: params[:permalink])
+      else
+        @category = categories.where(members_only: false).find_by(permalink: params[:permalink])
+      end
+      @posts = @category.posts.page(params[:page]).per(10)
     end
+
+    def index
+      @categoris = Category.published.ordered
+      @categoris = @categoris.where(members_only: false) unless user_signed_in?
+      @categoris = @categoris.page(params[:page]).per(30)
+    end
+
   end
 end
--- app/controllers/faalis/blog/posts_controller.rb
+++ app/controllers/faalis/blog/posts_controller.rb
@@ -6,12 +6,21 @@ module Faalis::Blog
     layout Engine.default_layout
 
     def index
-      @posts = Post.published.ordered.page(params[:page]).per(5)
+      @posts = Post.published.ordered
+      @posts = @posts.where(members_only: false) unless user_signed_in?
+      @posts = @posts.page(params[:page]).per(5)
     end
 
     def show
-      @post = Post.published.find_by(permalink: params[:permalink])
+      posts = Post.published
 
+      if user_signed_in?
+        @post = posts.find_by(permalink: params[:permalink])
+
+      else
+        @post = posts.where(members_only: false) \
+                .find_by(permalink: params[:permalink])
+      end
     end
   end
 end
