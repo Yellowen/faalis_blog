7923050c97c29e93d229b9b6b9732325e2318523
sadmad <sarmad.re@gmail.com>
2016-06-14 16:24:56 +0430

    changed messages#show to message#show in routs.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Faalis::Routes.draw(::Faalis::Blog::Engine) do
 
   plugin do
     get "#{Faalis::Blog::Engine.posts_url_prefix}", to: 'posts#index', as: :index
-    get "#{Faalis::Blog::Engine.posts_url_prefix}/:permalink", to: 'posts#show', as: :post_show
+    get "#{Faalis::Blog::Engine.posts_url_prefix}/:permalink", to: 'post#show', as: :post_show
     get "#{Faalis::Blog::Engine.categories_url_prefix}/:permalink", to: 'categories#show', as: :category_show
   end
 
